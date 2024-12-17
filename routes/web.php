aaa690ecc8e81baad9ced3de1793c366e71dd5aa
Gajendra Chilhate <gajendra.chilhate@brainvire.com>
2024-12-17 12:57:48 +0530

    message feature added along with event and job

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () { return view('welcome'); });
+
+Route::get('/messages', [HomeController::class, 'messages'])->name('messages');
+
+Route::post('/message', [HomeController::class, 'message'])->name('message');
