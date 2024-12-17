bc4294b6cd6733cd493428dbf6e8702db668d04c
Gajendra Chilhate <gajendra.chilhate@brainvire.com>
2024-12-17 10:53:18 +0530

    initial commit

---
+++ app/Models/Message.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    protected $table = 'messages';
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'text'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function getTimeAttribute(): string {
+        return date(
+            "d M Y, H:i:s",
+            strtotime($this->attributes['created_at'])
+        );
+    }
+    
+}
